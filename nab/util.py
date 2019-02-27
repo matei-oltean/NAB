c9a7137e2a550c253c085d2127fe0adf84ca9c3c
matei-oltean <matei.oltean@gmail.com>
2019-02-27 09:07:15 +0100

    Convert code to Python3

--- nab/util.py
+++ nab/util.py
@@ -77,7 +77,7 @@ def updateFinalResults(newResults, resultsFilePath):
   """
   results = getOldDict(resultsFilePath)
 
-  for detector, score in newResults.iteritems():
+  for detector, score in newResults.items():
     results[detector] = score
 
   writeJSON(resultsFilePath, results)
@@ -101,13 +101,13 @@ def updateThresholds(newThresholds, thresholdsFilePath):
   """
   oldThresholds = getOldDict(thresholdsFilePath)
 
-  for detector, profileDictionary in newThresholds.iteritems():
+  for detector, profileDictionary in newThresholds.items():
     if detector not in oldThresholds:
       # add an entry for a new detector
       oldThresholds[detector] = newThresholds[detector]
       continue
 
-    for profileName, data in profileDictionary.iteritems():
+    for profileName, data in profileDictionary.items():
       if profileName not in oldThresholds[detector]:
         # add an entry for a new scoring profile under this detector
         oldThresholds[detector][profileName] = data
@@ -223,7 +223,7 @@ def osPathSplit(path, debug=False):
   while True:
     newpath, tail = os.path.split(path)
     if debug:
-      print repr(path), (newpath, tail)
+      print(repr(path), (newpath, tail))
     if newpath == path:
       assert not tail
       if path:
@@ -265,7 +265,7 @@ def flattenDict(dictionary, files={}, head=""):
 
   @param head       (string)  Prefix to each key
   """
-  for key in dictionary.keys():
+  for key in list(dictionary.keys()):
     concat = head + "/" + key if head != "" else key
     if type(dictionary[key]) is dict:
       flattenDict(dictionary[key], files, concat)
@@ -303,7 +303,7 @@ def recur(function, value, n):
   @param n        (int)         Number of times to recurse.
   """
   if n < 0 or int(n) != n:
-    print "incorrect input"
+    print("incorrect input")
     sys.exit()
 
   elif n == 0:
